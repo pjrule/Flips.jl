47b2a01f9c163ba61fb50d4de7b63002a29a650f
Parker J. Rule <parker.rule@tufts.edu>
2020-01-22 12:48:49 -0500

    Add initial assignment to pychain output

--- src/pychain.jl
+++ src/pychain.jl
@@ -1,6 +1,6 @@
 """Wrapper for Python calls."""
 function pychain(graph::IndexedGraph, plan::Plan, n_steps::Int, reversible::Bool,
-                 min_pop::Int, max_pop::Int, twister::MersenneTwister)::Array{Dict}
+                 min_pop::Int, max_pop::Int, twister::MersenneTwister)::Dict
     if reversible
         proposal = reversible_recom_until_step
     else
@@ -8,6 +8,8 @@ function pychain(graph::IndexedGraph, plan::Plan, n_steps::Int, reversible::Bool
     end
     steps = Array{Dict}(undef, 0)
     step_count = 0
+    initial_assignment = Dict(node - 1 => assignment for (node, assignment)
+                              in enumerate(plan.assignment))
     while step_count < n_steps
         flip, self_loops, reasons = proposal(graph, plan, min_pop, max_pop, twister)
         update!(plan, graph, flip)
@@ -20,5 +22,6 @@ function pychain(graph::IndexedGraph, plan::Plan, n_steps::Int, reversible::Bool
                           "district_adj" => plan.district_adj))
         step_count += (self_loops + 1)
     end
-    return steps
+    return Dict("initial_assignment" => initial_assignment,
+                "steps" => steps)
 end
